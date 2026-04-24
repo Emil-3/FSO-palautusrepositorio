fd627c8feb6ab7259d0cac60726a02a33d4d2322
Emil Huuki <116680128+Emil-06737@users.noreply.github.com>
2026-04-24 16:53:48 +0300

    Do exercise 4 in part 1

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -22,24 +22,26 @@ const Part = ({ name, exercises}) => (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content content={[part1, part2, part3]}/>
-      <Total sum={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content content={parts}/>
+      <Total sum={parts.map(x => x.exercises).reduce((x, y) => x + y)}/>
     </div>
   )
 }
