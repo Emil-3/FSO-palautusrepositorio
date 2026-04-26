12a9637df3ac320922ba078964451d78f46d7f40
Emil Huuki <116680128+Emil-06737@users.noreply.github.com>
2026-04-26 19:25:19 +0300

    Do exercise 2.7

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -16,6 +16,10 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
+    if (persons.map(person => person.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
     const newPersons = [...persons]
     newPersons.push({ name: newName })
     setPersons(newPersons)
