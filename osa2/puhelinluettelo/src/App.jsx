63a18badd3b15d5d579c16cfe85ed5f10e274f7a
Emil Huuki <116680128+Emil-06737@users.noreply.github.com>
2026-04-26 18:35:42 +0300

    Do exercise 2.6

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const Person = ({ person }) => {
+
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPersons = [...persons]
+    newPersons.push({ name: newName })
+    setPersons(newPersons)
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+    </div>
+  )
+
+}
+
+export default App
