32f3f766dc9564233d93f424879fccbf5a047cec
Emil Huuki <116680128+Emil-06737@users.noreply.github.com>
2026-04-26 21:33:54 +0300

    Do exercise 2.10

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-
-const Person = ({ person }) => {
-
-}
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -42,26 +41,13 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with<input value={filter} onChange={handleFilter} />
-      </div>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name:<input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number:<input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-        {filteredPersons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      <Filter filter={filter} onChange={handleFilter} />
+      <h3>Add a new</h3>
+      <PersonForm onSubmit={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
+      <h3>Numbers</h3>
+      <Persons persons={filteredPersons} />
     </div>
   )
-
 }
 
 export default App
