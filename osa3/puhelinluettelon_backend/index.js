df85e8c2cd475c772551b4d1f3bc5aad557bb5ae
Emil Huuki <116680128+Emil-06737@users.noreply.github.com>
2026-05-01 18:54:03 +0300

    Do exercise 3.3

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -28,6 +28,16 @@ app.get('/api/persons', (req, res) => {
     res.json(phoneNumberInfo)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = phoneNumberInfo.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
 app.get('/info', (req, res) => {
     res.send(
         `<div>
