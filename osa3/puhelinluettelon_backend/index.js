08b7c0352640c4f008cf65b3fb11d493e01b33bb
Emil Huuki <116680128+Emil-3@users.noreply.github.com>
2026-08-01 18:08:46 +0300

    Do exercise 3.11

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const cors = require('cors')
 
 let phoneNumberInfo = [
     {
@@ -26,7 +25,7 @@ let phoneNumberInfo = [
     }
 ]
 
-app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 morgan.token('data', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
